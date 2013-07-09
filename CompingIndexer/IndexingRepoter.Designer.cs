705924013439b7c5701802e3e82178fc6a3c5625
Martin McGirk <martin@mcgirk.com>
2013-07-09 17:32:47 +0100

    Initial Commit
    Analyses the first page of competitions listed on a comping forum and
    lets me click on the competition I fancy and be taken to both the forum
    page and the website that the competition is actually on.

---
+++ CompingIndexer/IndexingRepoter.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CompingIndexer
+{
+    partial class IndexingRepoter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AvailableCompetitions = new System.Windows.Forms.ListBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.webBrowserActual = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AvailableCompetitions
+            // 
+            this.AvailableCompetitions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AvailableCompetitions.FormattingEnabled = true;
+            this.AvailableCompetitions.Location = new System.Drawing.Point(0, 0);
+            this.AvailableCompetitions.Name = "AvailableCompetitions";
+            this.AvailableCompetitions.Size = new System.Drawing.Size(304, 625);
+            this.AvailableCompetitions.TabIndex = 0;
+            this.AvailableCompetitions.SelectedIndexChanged += new System.EventHandler(this.AvailableCompetitions_SelectedIndexChanged);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(606, 312);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.AvailableCompetitions);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(914, 625);
+            this.splitContainer1.SplitterDistance = 304;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.webBrowser);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.webBrowserActual);
+            this.splitContainer2.Size = new System.Drawing.Size(606, 625);
+            this.splitContainer2.SplitterDistance = 312;
+            this.splitContainer2.TabIndex = 2;
+            // 
+            // webBrowserActual
+            // 
+            this.webBrowserActual.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowserActual.Location = new System.Drawing.Point(0, 0);
+            this.webBrowserActual.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserActual.Name = "webBrowserActual";
+            this.webBrowserActual.ScriptErrorsSuppressed = true;
+            this.webBrowserActual.Size = new System.Drawing.Size(606, 309);
+            this.webBrowserActual.TabIndex = 0;
+            // 
+            // IndexingRepoter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 625);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "IndexingRepoter";
+            this.Text = "Comping Indexer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox AvailableCompetitions;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.WebBrowser webBrowserActual;
+    }
+}
+
---
+++ CompingIndexer/IndexingRepoter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompingIndexer
+{
+    public partial class IndexingRepoter : Form
+    {
+        public IndexingRepoter()
+        {
+            InitializeComponent();
+            HarvestCompetitionLinks(@"http://forums.moneysavingexpert.com/forumdisplay.php?s=e47e6ec28f39dc7ec25a369e335606d8&f=72");       
+        }
+
+        private void HarvestCompetitionLinks(String path)
+        {
+            String source = GetPageSource(path);
+            List<LinkItem> threadsOnPage = HarvestThreadLinks(source);
+            AvailableCompetitions.DataSource = threadsOnPage;
+            AvailableCompetitions.ValueMember = "Url";
+            AvailableCompetitions.DisplayMember = "Title";
+        }
+
+        /// <summary>
+        /// Parses out links to MSE forum threads. 
+        /// </summary>
+        /// <param name="source">The source of the page</param>
+        /// <returns>A list of URLs</returns>
+        private List<LinkItem> HarvestThreadLinks(String source)
+        {
+            HashSet<LinkItem> linksFound = new HashSet<LinkItem>(); //Hashset for speed of lookup
+
+            Regex regex = new Regex("href=\"showthread.+(t=[0-9]+)\".*[>](.+)[<]", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var matches = regex.Matches(source);
+            foreach (Match match in matches)
+            {
+                String stub = "http://forums.moneysavingexpert.com/showthread.php?";
+                String newThread = String.Format("{0}{1}", stub, match.Groups[1].Value);
+                LinkItem li = new LinkItem(newThread, match.Groups[2].Value);
+                if (!linksFound.Contains(li) && li.Title.StartsWith("E:"))
+                {
+                    linksFound.Add(li);
+                }
+            }
+
+            return linksFound.ToList();
+        }
+
+        private LinkItem HarvestCompetitionLink(String link, String title)
+        {
+            String source = GetPageSource(link);
+            String trimmedSource = source.Substring(source.IndexOf(@"<!-- message -->"));
+            Regex regex = new Regex("href=\"(http://[^\"]+)\"", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var matches = regex.Matches(trimmedSource);
+            return new LinkItem(matches[0].Groups[1].Value, title);
+        }
+
+        /// <summary>
+        /// Downloads the source of a webpage and returns it as a string for processing
+        /// </summary>
+        /// <param name="url">The URL of the page to get the source from</param>
+        /// <returns>The source of the url as a string.</returns>
+        private String GetPageSource(String url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+            //return File.ReadAllText(@"C:\TestCompPage.txt");
+        }
+
+        private void AvailableCompetitions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LinkItem chosenLink = (LinkItem)AvailableCompetitions.SelectedItem;
+            webBrowser.Navigate(chosenLink.Url);
+            LinkItem actualLink = HarvestCompetitionLink(chosenLink.Url, chosenLink.Title);
+            webBrowserActual.Navigate(actualLink.Url);
+        }
+
+       
+    }
+
+    class LinkItem
+    {
+        public String Url { get; set; }
+        public String Title { get; set; }
+        public LinkItem(String url, String title)
+        {
+            Url = url;
+            Title = title;
+        }
+    }
+}
